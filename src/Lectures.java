f5e66d11a8e4561f83ecb5a1eb815330d36d813a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-10 11:46:47 +0200

    L02 Add program termination only by user

--- src/Lectures.java
+++ src/Lectures.java
@@ -8,69 +8,65 @@ public class Lectures {
         int[] commands = {1, 2, 3, 4, 5};
         int command;
 
-        do {
-            System.out.print("Input command (1-5): ");
-            command = scanner.nextInt();
-        } while (Arrays.binarySearch(commands, command) < 0);
-
         String[] lectures = {"Intro", "Basics", "Object-oriented Programming", "Error Propagation and Handling"};
 
-        if (command == 1) {
-            for (String lecture : lectures) {
-                System.out.println(lecture);
-            }
-        } else if (command == 2) {
-            System.out.print("Input new lecture: ");
-            String lecture = scanner.next();
-            String[] newLectures = new String[lectures.length + 1];
-
-            for (int i = 0; i < lectures.length; i++) {
-                newLectures[i] = lectures[i];
-            }
-
-            newLectures[newLectures.length - 1] = lecture;
-            lectures = newLectures;
-
-            for (String l : lectures) {
-                System.out.println(l);
-            }
-        } else if (command == 3) {
-            int numberOfLecture;
-
+        while (true) {
             do {
-                System.out.print("Input number of lecture: ");
-                numberOfLecture = scanner.nextInt();
-            } while (numberOfLecture < 0 || numberOfLecture >= lectures.length);
+                System.out.print("Input command (1-5): ");
+                command = scanner.nextInt();
+            } while (Arrays.binarySearch(commands, command) < 0);
 
-            String[] newLectures = new String[lectures.length - 1];
+            if (command == 1) {
+                for (String lecture : lectures) {
+                    System.out.println(lecture);
+                }
+                System.out.println();
+            } else if (command == 2) {
+                System.out.print("Input new lecture: ");
+                String lecture = scanner.next();
+                String[] newLectures = new String[lectures.length + 1];
 
-            for (int i = 0; i < numberOfLecture; i++) {
-                newLectures[i] = lectures[i];
+                for (int i = 0; i < lectures.length; i++) {
+                    newLectures[i] = lectures[i];
+                }
+
+                newLectures[newLectures.length - 1] = lecture;
+                lectures = newLectures;
+                System.out.println();
+            } else if (command == 3) {
+                int numberOfLecture;
+
+                do {
+                    System.out.print("Input number of lecture: ");
+                    numberOfLecture = scanner.nextInt();
+                } while (numberOfLecture < 0 || numberOfLecture >= lectures.length);
+
+                String[] newLectures = new String[lectures.length - 1];
+
+                for (int i = 0; i < numberOfLecture; i++) {
+                    newLectures[i] = lectures[i];
+                }
+
+                for (int i = numberOfLecture + 1; i < lectures.length; i++) {
+                    newLectures[i - 1] = lectures[i];
+                }
+
+                lectures = newLectures;
+                System.out.println();
+            } else if (command == 4) {
+                int numberOfLecture;
+
+                do {
+                    System.out.print("Input number of lecture: ");
+                    numberOfLecture = scanner.nextInt();
+                } while (numberOfLecture < 0 || numberOfLecture >= lectures.length);
+
+                System.out.println(lectures[numberOfLecture]);
+                System.out.println();
+            } else if (command == 5) {
+                scanner.close();
+                System.exit(0);
             }
-
-            for (int i = numberOfLecture + 1; i < lectures.length; i++) {
-                newLectures[i - 1] = lectures[i];
-            }
-
-            lectures = newLectures;
-
-            for (String l : lectures) {
-                System.out.println(l);
-            }
-        } else if (command == 4) {
-            int numberOfLecture;
-
-            do {
-                System.out.print("Input number of lecture: ");
-                numberOfLecture = scanner.nextInt();
-            } while (numberOfLecture < 0 || numberOfLecture >= lectures.length);
-
-            System.out.println(lectures[numberOfLecture]);
-        } else if (command == 5) {
-            scanner.close();
-            System.exit(0);
         }
-
-        scanner.close();
     }
 }
