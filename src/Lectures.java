089f648ab542d85a5d8967816ade64c1aaf17379
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-10 11:16:21 +0200

    L02 Add a selection of commands

--- src/Lectures.java
+++ src/Lectures.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Lectures {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int[] commands = {1, 2, 3, 4, 5};
+        int command;
+
+        do {
+            System.out.print("Input command (1-5): ");
+            command = scanner.nextInt();
+        } while (Arrays.binarySearch(commands, command) < 0);
+
+        scanner.close();
     }
 }
