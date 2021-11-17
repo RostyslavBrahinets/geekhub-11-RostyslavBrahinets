8d4737d64b5f8c43e8e4e55c0ed44a0f9914205a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-17 18:04:14 +0200

    L02 Split code to logical separate methods

--- src/Lectures.java
+++ src/Lectures.java
@@ -2,85 +2,106 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lectures {
+    static final Scanner scanner = new Scanner(System.in);
+    static String[] lectures = {"Intro", "Basics", "Object-oriented Programming", "Error Propagation and Handling"};
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String[] commands = {"1", "2", "3", "4", "5"};
-        String command;
-
-        String[] lectures = {"Intro", "Basics", "Object-oriented Programming", "Error Propagation and Handling"};
-
         while (true) {
-            do {
-                System.out.printf("%nInput command (1-5): ");
-                command = scanner.nextLine();
-            } while (Arrays.binarySearch(commands, command) < 0);
-
-            switch (command) {
+            switch (getCommand()) {
                 case "1" -> {
-                    for (String lecture : lectures) {
-                        System.out.println(lecture);
-                    }
+                    showAllLectures();
                 }
                 case "2" -> {
-                    System.out.print("Input new lecture: ");
-                    String lecture = scanner.nextLine();
-                    String[] newLectures = new String[lectures.length + 1];
-
-                    for (int i = 0; i < lectures.length; i++) {
-                        newLectures[i] = lectures[i];
-                    }
-
-                    newLectures[newLectures.length - 1] = lecture;
-                    lectures = newLectures;
+                    addNewLecture();
                 }
                 case "3" -> {
-                    String[] numbersOfLectures = new String[lectures.length];
-
-                    for (int i = 0; i < lectures.length; i++) {
-                        numbersOfLectures[i] = String.valueOf(i);
-                    }
-
-                    String numberOfLecture;
-
-                    do {
-                        System.out.print("Input number of lecture: ");
-                        numberOfLecture = scanner.nextLine();
-                    } while (Arrays.binarySearch(numbersOfLectures, numberOfLecture) < 0);
-
-                    String[] newLectures = new String[lectures.length - 1];
-
-                    for (int i = 0; i < Integer.parseInt(numberOfLecture); i++) {
-                        newLectures[i] = lectures[i];
-                    }
-
-                    for (int i = Integer.parseInt(numberOfLecture) + 1; i < lectures.length; i++) {
-                        newLectures[i - 1] = lectures[i];
-                    }
-
-                    lectures = newLectures;
+                    deleteLectureByNumber();
                 }
                 case "4" -> {
-                    String[] numbersOfLectures = new String[lectures.length];
-
-                    for (int i = 0; i < lectures.length; i++) {
-                        numbersOfLectures[i] = String.valueOf(i);
-                    }
-
-                    String numberOfLecture;
-
-                    do {
-                        System.out.print("Input number of lecture: ");
-                        numberOfLecture = scanner.nextLine();
-                    } while (Arrays.binarySearch(numbersOfLectures, numberOfLecture) < 0);
-
-                    System.out.println(lectures[Integer.parseInt(numberOfLecture)]);
+                    showLectureByNumber();
                 }
                 case "5" -> {
-                    scanner.close();
-                    System.exit(0);
+                    exit();
                 }
             }
         }
+    }
+
+    private static String getCommand() {
+        String[] commands = {"1", "2", "3", "4", "5"};
+        String command;
+
+        do {
+            System.out.printf("%nInput command (1-5): ");
+            command = scanner.nextLine();
+        } while (Arrays.binarySearch(commands, command) < 0);
+
+        return command;
+    }
+
+    private static void showAllLectures() {
+        for (String lecture : lectures) {
+            System.out.println(lecture);
+        }
+    }
+
+    private static void addNewLecture() {
+        System.out.print("Input new lecture: ");
+        String lecture = scanner.nextLine();
+        String[] newLectures = new String[lectures.length + 1];
+
+        for (int i = 0; i < lectures.length; i++) {
+            newLectures[i] = lectures[i];
+        }
+
+        newLectures[newLectures.length - 1] = lecture;
+        lectures = newLectures;
+    }
+
+    private static void deleteLectureByNumber() {
+        String numberOfLecture = getNumberOfLecture();
+
+        String[] newLectures = new String[lectures.length - 1];
+
+        for (int i = 0; i < Integer.parseInt(numberOfLecture); i++) {
+            newLectures[i] = lectures[i];
+        }
+
+        for (int i = Integer.parseInt(numberOfLecture) + 1; i < lectures.length; i++) {
+            newLectures[i - 1] = lectures[i];
+        }
+
+        lectures = newLectures;
+    }
+
+    private static void showLectureByNumber() {
+        String numberOfLecture = getNumberOfLecture();
+        System.out.println(lectures[Integer.parseInt(numberOfLecture)]);
+    }
+
+    private static void exit() {
+        scanner.close();
+        System.exit(0);
+    }
+
+    private static String[] getArrayNumbersOfLectures() {
+        String[] numbersOfLectures = new String[lectures.length];
+
+        for (int i = 0; i < lectures.length; i++) {
+            numbersOfLectures[i] = String.valueOf(i);
+        }
+
+        return numbersOfLectures;
+    }
+
+    private static String getNumberOfLecture() {
+        String numberOfLecture;
+
+        do {
+            System.out.print("Input number of lecture: ");
+            numberOfLecture = scanner.nextLine();
+        } while (Arrays.binarySearch(getArrayNumbersOfLectures(), numberOfLecture) < 0);
+
+        return numberOfLecture;
     }
 }
