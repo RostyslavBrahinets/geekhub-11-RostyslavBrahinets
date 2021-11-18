256dbe20fdd990183780def9a7370f7c83ebf8cf
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-18 09:09:03 +0200

    L03 Create class 'Lecture'

---
+++ src/Lecture.java
@@ -0,0 +1,15 @@
+public class Lecture {
+    private String title;
+
+    public Lecture(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
