2cafb52d872ffcb77d8a7ae03ca254487f3f200e
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-18 12:15:31 +0200

    L03 Create 'Person'

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private String slackNickname;
+    private String gitHubNickname;
+
+    public Person(String firstName, String lastName, String slackNickname, String gitHubNickname) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.slackNickname = slackNickname;
+        this.gitHubNickname = gitHubNickname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSlackNickname() {
+        return slackNickname;
+    }
+
+    public void setSlackNickname(String slackNickname) {
+        this.slackNickname = slackNickname;
+    }
+
+    public String getGitHubNickname() {
+        return gitHubNickname;
+    }
+
+    public void setGitHubNickname(String gitHubNickname) {
+        this.gitHubNickname = gitHubNickname;
+    }
+}
