744ed32ed0104260ae309d667d6c5facd60ff85b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-18 11:23:20 +0200

    L03 Create 'Homework'

---
+++ src/Homework.java
@@ -0,0 +1,66 @@
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Homework {
+    private String task;
+    private LocalDateTime deadline;
+    private Lecturer lecturer;
+
+    public Homework(String task, LocalDateTime deadline, Lecturer lecturer) {
+        this.task = task;
+        this.deadline = deadline;
+        this.lecturer = lecturer;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Homework homework = (Homework) o;
+        return Objects.equals(task, homework.task) && Objects.equals(deadline, homework.deadline) && Objects.equals(lecturer, homework.lecturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(task, deadline, lecturer);
+    }
+
+    @Override
+    public String toString() {
+        return "Homework{" +
+            "task='" + task + '\'' +
+            ", deadline=" + deadline +
+            ", lecturer=" + lecturer +
+            '}';
+    }
+}
