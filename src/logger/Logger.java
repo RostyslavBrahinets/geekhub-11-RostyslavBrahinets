c657c817f029a27bf0c6925d54ee71168121b209
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-24 15:44:53 +0200

    L04 Update Logger

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -2,10 +2,10 @@ package logger;
 
 public class Logger {
     public static void log(LogType type, String className, String message) {
-        System.out.printf("%n[%s] %s: %s%n", type, className, message);
+        System.out.printf("[%s] %s: %s%n", type, className, message);
     }
 
     public static void log(LogType type, String className, String message, Exception e) {
-        System.out.printf("%n[%s] %s: %s%n%s%n", type, className, message, e.getStackTrace()[0]);
+        System.out.printf("[%s] %s: %s%n%s%n", type, className, message, e.getStackTrace()[0]);
     }
 }
