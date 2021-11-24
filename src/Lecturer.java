32ca913c506469aa9b526b55e6d06fb5bc866966
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-11-24 11:17:32 +0200

    L03 Delete 'Lecturer'

--- src/Lecturer.java
+++
@@ -1,6 +0,0 @@
-public class Lecturer extends Person {
-    public Lecturer(String firstName, String lastName, String slackNickname,
-                    String gitHubNickname) {
-        super(firstName, lastName, slackNickname, gitHubNickname);
-    }
-}
