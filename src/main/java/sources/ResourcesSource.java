f76ae763ac4d03bed53f2bcc28d2cd85ca2d1791
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-12 12:10:30 +0200

    L03 Add ResourceSource

---
+++ src/main/java/sources/ResourcesSource.java
@@ -0,0 +1,61 @@
+package sources;
+
+import exceptions.ResourceNotFoundException;
+import exceptions.ValidationException;
+import models.Resource;
+
+import java.util.Arrays;
+
+public class ResourcesSource {
+    private static ResourcesSource instance;
+    private Resource[] resources;
+
+    public ResourcesSource() {
+        resources = new Resource[0];
+    }
+
+    public Resource[] getResources() {
+        return resources;
+    }
+
+    public void addResource(Resource resource) {
+        if (resource == null || resource.getName().isBlank()) {
+            throw new ValidationException("Resource is invalid");
+        }
+
+        resources = Arrays.copyOf(resources, resources.length + 1);
+        resources[resources.length - 1] = resource;
+    }
+
+    public void deleteResource(int id) {
+        if (id < 0 || id >= resources.length) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+
+        Resource[] newResources = new Resource[resources.length - 1];
+
+        System.arraycopy(resources, 0, newResources, 0, id);
+
+        if (resources.length - (id + 1) >= 0) {
+            System.arraycopy(resources, id + 1, newResources, id, resources.length - (id + 1));
+        }
+
+        resources = newResources;
+    }
+
+    public Resource getResourceById(int id) {
+        if (id < 0 || id >= resources.length) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+
+        return resources[id];
+    }
+
+    public static ResourcesSource getInstance() {
+        if (instance == null) {
+            instance = new ResourcesSource();
+        }
+
+        return instance;
+    }
+}
