73e80c9f68c70bbd84ea47fa6b49969c96ee9b8c
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-12 11:11:02 +0200

    L04 Refactor LessonNotFoundException

--- src/main/java/exceptions/LessonNotFoundException.java
+++ src/main/java/exceptions/LessonNotFoundException.java
@@ -1,11 +1,10 @@
 package exceptions;
 
-public class LessonNotFoundException extends ArrayIndexOutOfBoundsException {
+public class LessonNotFoundException extends RuntimeException {
     public LessonNotFoundException() {
     }
 
     public LessonNotFoundException(String message) {
         super(message);
     }
-
 }
