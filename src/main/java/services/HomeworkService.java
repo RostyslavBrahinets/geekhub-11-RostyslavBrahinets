55a0b9623e4467ad8bfd107ac2bd38de1d94d192
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-15 20:55:00 +0200

    L03 Create HomeworkService

---
+++ src/main/java/services/HomeworkService.java
@@ -0,0 +1,50 @@
+package services;
+
+import exceptions.NotFoundException;
+import exceptions.ValidationException;
+import logger.Logger;
+import models.Homework;
+import sources.HomeworkSource;
+import validators.HomeworkValidator;
+
+import java.util.List;
+
+public class HomeworkService {
+    private final HomeworkSource homeworkSource = HomeworkSource.getInstance();
+    private final HomeworkValidator validator = new HomeworkValidator();
+
+    public List<Homework> getHomeworks() {
+        return homeworkSource.getHomeworks();
+    }
+
+    public void addHomework(String task) {
+        try {
+            validator.validate(task);
+            homeworkSource.addHomework(new Homework(task));
+        } catch (ValidationException e) {
+            Logger.error(getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+    public void deleteHomework(int id) {
+        try {
+            validator.validate(id);
+            homeworkSource.deleteHomework(id);
+        } catch (NotFoundException e) {
+            Logger.error(getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+    public Homework getHomework(int id) {
+        Homework homework = null;
+
+        try {
+            validator.validate(id);
+            homework = homeworkSource.getHomework(id);
+        } catch (NotFoundException e) {
+            Logger.error(getClass().getName(), e.getMessage(), e);
+        }
+
+        return homework;
+    }
+}
