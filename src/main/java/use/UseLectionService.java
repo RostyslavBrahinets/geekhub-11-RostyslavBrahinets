520c5d627c9c812765d5ca3f4b443ae9dfe0c204
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-15 22:17:19 +0200

    L03 Change UseLectionService

--- src/main/java/use/UseLectionService.java
+++ src/main/java/use/UseLectionService.java
@@ -1,8 +1,12 @@
 package use;
 
 import logger.Logger;
+import models.Homework;
 import models.Lection;
+import models.Person;
+import services.HomeworkService;
 import services.LectionService;
+import services.PersonService;
 import services.ResourceService;
 
 import java.util.InputMismatchException;
@@ -12,19 +16,33 @@ public class UseLectionService {
     private final Scanner scanner = new Scanner(System.in);
     private final LectionService lectionService = new LectionService();
     private final UseResourceService useResourceService = new UseResourceService();
+    private final UsePersonService usePersonService = new UsePersonService();
+    private final UseHomeworkService useHomeworkService = new UseHomeworkService();
 
     public void showAllLections() {
         for (Lection lection : lectionService.getLections()) {
             Logger.info(getClass().getName(),
-                String.format("%s: %s%n", lection.getName(), lection.getResources()));
+                String.format("%s: %s; %s; %s %s; %s%n",
+                    lection.getName(),
+                    lection.getDescribe(),
+                    lection.getResources(),
+                    lection.getLecturer().getFirstName(),
+                    lection.getLecturer().getLastName(),
+                    lection.getHomework().getTasks()
+                ));
         }
     }
 
     public void addNewLection() {
         String name = getNameOfLection();
+        String describe = getDescribeOfLection();
         useResourceService.addResources(getCountOfResources());
+        Person lecturer = getLecturer();
+        Homework homework = getHomework();
+
         ResourceService resourceService = new ResourceService();
-        lectionService.addLection(name, resourceService.getResources());
+        lectionService.addLection(name, describe, resourceService.getResources(),
+            lecturer, homework);
     }
 
     public void deleteLectionById() {
@@ -34,34 +52,34 @@ public class UseLectionService {
     public void showLectionById() {
         Lection lection = lectionService.getLection(getIdOfLection());
         if (lection != null) {
-            Logger.info(getClass().getName(), lection.getName());
+            Logger.info(getClass().getName(),
+                String.format("%s: %s; %s; %s %s; %s%n",
+                    lection.getName(),
+                    lection.getDescribe(),
+                    lection.getResources(),
+                    lection.getLecturer().getFirstName(),
+                    lection.getLecturer().getLastName(),
+                    lection.getHomework().getTasks()
+                ));
         }
     }
 
     public void exit() {
         scanner.close();
         useResourceService.closeScanner();
+        usePersonService.closeScanner();
+        useHomeworkService.closeScanner();
         System.exit(0);
     }
 
     private String getNameOfLection() {
-        System.out.print("Input name of lection: ");
+        System.out.print("\nInput name of lection: ");
         return scanner.nextLine();
     }
 
-    private int getIdOfLection() {
-        System.out.print("Input id of lection: ");
-        int id;
-
-        try {
-            id = scanner.nextInt();
-            scanner.nextLine();
-        } catch (InputMismatchException e) {
-            id = -1;
-            scanner.nextLine();
-        }
-
-        return id;
+    private String getDescribeOfLection() {
+        System.out.print("Input describe of lection: ");
+        return scanner.nextLine();
     }
 
     private int getCountOfResources() {
@@ -78,5 +96,30 @@ public class UseLectionService {
         }
 
         return count;
+    }
+
+    private Person getLecturer() {
+        usePersonService.addPeople(1);
+        return new PersonService().getPerson(0);
+    }
+
+    private Homework getHomework() {
+        useHomeworkService.addHomeworks(1);
+        return new HomeworkService().getHomework(0);
+    }
+
+    private int getIdOfLection() {
+        System.out.print("Input id of lection: ");
+        int id;
+
+        try {
+            id = scanner.nextInt();
+            scanner.nextLine();
+        } catch (InputMismatchException e) {
+            id = -1;
+            scanner.nextLine();
+        }
+
+        return id;
     }
 }
