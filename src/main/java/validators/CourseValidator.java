763f3265fc5b7b26e056f69737d403f9677e8bef
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-15 21:25:01 +0200

    L03 Create CourseValidator

---
+++ src/main/java/validators/CourseValidator.java
@@ -0,0 +1,28 @@
+package validators;
+
+import exceptions.NotFoundException;
+import exceptions.ValidationException;
+import models.Lection;
+import models.Person;
+import sources.CourseSource;
+
+import java.util.List;
+
+public class CourseValidator {
+    public void validate(String name, List<Lection> lections, List<Person> students) {
+        if (name == null || name.isBlank()) {
+            throw new ValidationException("Name of course is invalid");
+        } else if (lections == null) {
+            throw new ValidationException("List of lections is invalid");
+        } else if (students == null) {
+            throw new ValidationException("List of students is invalid");
+        }
+    }
+
+    public void validate(int id) {
+        CourseSource courseSource = CourseSource.getInstance();
+        if (id < 0 || id >= courseSource.getCourses().size()) {
+            throw new NotFoundException("Course not found");
+        }
+    }
+}
