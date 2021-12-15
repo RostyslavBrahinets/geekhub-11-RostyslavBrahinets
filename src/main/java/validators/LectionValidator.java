940fa4b3269db283418d960c97f182fce6f8a219
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-15 16:44:00 +0200

    L07 Create LectionValidator

---
+++ src/main/java/validators/LectionValidator.java
@@ -0,0 +1,25 @@
+package validators;
+
+import exceptions.LessonNotFoundException;
+import exceptions.ValidationException;
+import models.Resource;
+import sources.LectionSource;
+
+import java.util.List;
+
+public class LectionValidator {
+    public void validate(String name, List<Resource> resources) {
+        if (name == null || name.isBlank()) {
+            throw new ValidationException("Name of lection is invalid");
+        } else if (resources == null) {
+            throw new ValidationException("Resources are invalid");
+        }
+    }
+
+    public void validate(int id) {
+        LectionSource lectionSource = LectionSource.getInstance();
+        if (id < 0 || id >= lectionSource.getLections().size()) {
+            throw new LessonNotFoundException("Lection not found");
+        }
+    }
+}
