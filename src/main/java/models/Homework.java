75872d459dfc16d6cdfe0a7351012554b1684e2b
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-16 00:24:36 +0200

    L03 Add equals(), hashCode() and toString() to Homework

--- src/main/java/models/Homework.java
+++ src/main/java/models/Homework.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Homework {
     private List<String> tasks;
@@ -15,5 +16,23 @@ public class Homework {
 
     public void setTasks(List<String> tasks) {
         this.tasks = tasks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Homework homework = (Homework) o;
+        return Objects.equals(tasks, homework.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tasks);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Homework {%s}", tasks);
     }
 }
