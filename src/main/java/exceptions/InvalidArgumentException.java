593baa4e40f118b23bccb1f6709f62da0fd5e3c8
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-15 18:07:39 +0200

    L04 Create InvalidArgumentException

---
+++ src/main/java/exceptions/InvalidArgumentException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class InvalidArgumentException extends RuntimeException {
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+
+    public InvalidArgumentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
