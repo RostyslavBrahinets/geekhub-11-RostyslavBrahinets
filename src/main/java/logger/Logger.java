c87141b2e1c7b68f87a532c10bcdd0e07e83a1de
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2021-12-23 21:24:51 +0200

    L08 Add data and time to Logger

--- src/main/java/logger/Logger.java
+++ src/main/java/logger/Logger.java
@@ -1,32 +1,44 @@
 package logger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Logger {
     private static final List<String> logs = new ArrayList<>();
+    private static LocalDateTime localDateTime;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss:ms");
+
+    private Logger() {
+        throw new IllegalStateException("Utility class");
+    }
 
     public static void info(String className, String message) {
-        String log = "[" + LogType.INFO + "] " + className + ": " + message;
+        localDateTime = LocalDateTime.now();
+        String log = "[" + LogType.INFO + "] " + className + ": " + message + "\n" + localDateTime.format(formatter);
         logs.add(log);
         System.out.println(log);
     }
 
     public static void warning(String className, String message) {
-        String log = "[" + LogType.WARNING + "] " + className + ": " + message;
+        localDateTime = LocalDateTime.now();
+        String log = "[" + LogType.WARNING + "] " + className + ": " + message + "\n" + localDateTime.format(formatter);
         logs.add(log);
         System.out.println(log);
     }
 
     public static void error(String className, String message) {
-        String log = "[" + LogType.ERROR + "] " + className + ": " + message;
+        localDateTime = LocalDateTime.now();
+        String log = "[" + LogType.ERROR + "] " + className + ": " + message + "\n" + localDateTime.format(formatter);
         logs.add(log);
         System.out.println(log);
     }
 
     public static void error(String className, String message, Exception e) {
-        String log = "[" + LogType.ERROR + "] " + className + ": " + message + "\n"
-            + e.getStackTrace()[0];
+        localDateTime = LocalDateTime.now();
+        String log = "[" + LogType.ERROR + "] " + className + ": " + message + "\n" + localDateTime.format(formatter)
+            + "\n" + e.getStackTrace()[0];
         logs.add(log);
         System.out.println(log);
     }
