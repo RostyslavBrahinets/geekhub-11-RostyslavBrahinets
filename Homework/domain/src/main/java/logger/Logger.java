f4abd7236d22309dff31c060a61caa60277c6c04
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-01-21 10:36:51 +0200

    L10 Implement possibility to save logs in file

--- Homework/domain/src/main/java/logger/Logger.java
+++ Homework/domain/src/main/java/logger/Logger.java
@@ -1,5 +1,8 @@
 package logger;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -11,41 +14,56 @@ public class Logger {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss:ms");
 
     private Logger() {
-        throw new IllegalStateException("Utility class");
     }
 
     public static void info(String className, String message) {
         localDateTime = LocalDateTime.now();
-        String log = "[" + LogType.INFO + "] " + className + ": " + message + "\n" + localDateTime.format(formatter);
+        String log = "[" + LogType.INFO + "] " + className + ": " + message + "\n"
+            + localDateTime.format(formatter) + "\n\n";
         logs.add(log);
+        writeLogToFile(log);
         System.out.println(log);
     }
 
     public static void warning(String className, String message) {
         localDateTime = LocalDateTime.now();
-        String log = "[" + LogType.WARNING + "] " + className + ": " + message + "\n" + localDateTime.format(formatter);
+        String log = "[" + LogType.WARNING + "] " + className + ": " + message + "\n"
+            + localDateTime.format(formatter) + "\n\n";
         logs.add(log);
+        writeLogToFile(log);
         System.out.println(log);
     }
 
     public static void error(String className, String message) {
         localDateTime = LocalDateTime.now();
-        String log = "[" + LogType.ERROR + "] " + className + ": " + message + "\n" + localDateTime.format(formatter);
+        String log = "[" + LogType.ERROR + "] " + className + ": " + message + "\n"
+            + localDateTime.format(formatter) + "\n\n";
         logs.add(log);
+        writeLogToFile(log);
         System.out.println(log);
     }
 
     public static void error(String className, String message, Exception e) {
         localDateTime = LocalDateTime.now();
         String log = "[" + LogType.ERROR + "] " + className + ": " + message + "\n" + localDateTime.format(formatter)
-            + "\n" + e.getStackTrace()[0];
+            + "\n" + e.getStackTrace()[0] + "\n\n";
         logs.add(log);
+        writeLogToFile(log);
         System.out.println(log);
     }
 
     public static void showAllLogs() {
         for (String log : logs) {
             System.out.println(log);
+        }
+    }
+
+    private static void writeLogToFile(String log) {
+        File file = new File("Homework/domain/src/main/resources/log.txt");
+        try (FileOutputStream out = new FileOutputStream(file, true)) {
+            out.write(log.getBytes());
+        } catch (IOException e) {
+            System.out.println(e.getMessage() + "\n" + e.getCause());
         }
     }
 }
