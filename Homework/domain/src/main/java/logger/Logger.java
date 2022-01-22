a44f177b21bac0509b1fdbb1428a9d4a9a20dcfb
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-01-22 11:12:48 +0200

    L10 Create interface 'Logger'

--- Homework/domain/src/main/java/logger/Logger.java
+++ Homework/domain/src/main/java/logger/Logger.java
@@ -1,80 +1,13 @@
 package logger;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+public interface Logger {
+    void info(String className, String message);
 
-public class Logger {
-    private static final List<Log> logs = new ArrayList<>();
-    private static LocalDateTime localDateTime;
+    void warning(String className, String message);
 
-    private Logger() {
-    }
+    void error(String className, String message);
 
-    public static void info(String className, String message) {
-        localDateTime = LocalDateTime.now();
-        Log log = new Log(LogType.INFO, className, message, localDateTime);
-        String logString = log.toString();
-        logs.add(log);
-        writeLogToFile(logString);
-        System.out.println(logString);
-    }
+    void error(String className, String message, Exception e);
 
-    public static void warning(String className, String message) {
-        localDateTime = LocalDateTime.now();
-        Log log = new Log(LogType.WARNING, className, message, localDateTime);
-        String logString = log.toString();
-        logs.add(log);
-        writeLogToFile(logString);
-        System.out.println(logString);
-    }
-
-    public static void error(String className, String message) {
-        localDateTime = LocalDateTime.now();
-        Log log = new Log(LogType.ERROR, className, message, localDateTime);
-        String logString = log.toString();
-        logs.add(log);
-        writeLogToFile(logString);
-        System.out.println(logString);
-    }
-
-    public static void error(String className, String message, Exception e) {
-        localDateTime = LocalDateTime.now();
-        Log log = new Log(LogType.ERROR, className, message, localDateTime, e);
-        String logString = log.toString();
-        logs.add(log);
-        writeLogToFile(logString);
-        System.out.println(logString);
-    }
-
-    public static void showAllLogs() {
-        for (Log log : logs) {
-            System.out.println(log);
-        }
-    }
-
-    public static void showLogsFromFile() {
-        File file = new File("Homework/domain/src/main/resources/logs.log");
-        try (FileInputStream in = new FileInputStream(file)) {
-            int readBytes;
-            while ((readBytes = in.read()) != -1) {
-                System.out.print((char) readBytes);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage() + "\n" + e.getCause());
-        }
-    }
-
-    private static void writeLogToFile(String log) {
-        File file = new File("Homework/domain/src/main/resources/logs.log");
-        try (FileOutputStream out = new FileOutputStream(file, true)) {
-            out.write(log.getBytes());
-        } catch (IOException e) {
-            System.out.println(e.getMessage() + "\n" + e.getCause());
-        }
-    }
+    void showLogs();
 }
