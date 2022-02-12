8ee15e6a2021140cdad3731cdafa3f71726f85f7
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-02-12 18:31:55 +0200

    L13 Create 'HomeWorksMenuServlet'

---
+++ Homework/web-app/src/main/java/org/geekhub/web/servlets/menu/homework/HomeWorksMenuServlet.java
@@ -0,0 +1,44 @@
+package org.geekhub.web.servlets.menu.homework;
+
+import org.geekhub.web.servlets.menu.MenuCommand;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/menu/homeworks")
+public class HomeWorksMenuServlet extends HttpServlet {
+    @Override
+    protected void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws IOException {
+        showMenu(request, response);
+    }
+
+    @Override
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws IOException {
+        MenuCommand.handleCommands(request, response);
+    }
+
+    private void showMenu(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws IOException {
+        response.setContentType("text/html");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write("<html><head><title>Home Works</title></head><body>");
+            writer.write("<h1>Home Works</h1>");
+            writer.write("<form action=\"homeworks\" method=\"post\">");
+            MenuCommand.showCommands(request, response);
+            writer.write("</form>");
+            writer.write("</body></html>");
+        }
+    }
+}
