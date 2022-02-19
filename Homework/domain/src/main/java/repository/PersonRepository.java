a2b8213cdac75db53921e32820c58e18177df581
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-02-19 19:07:18 +0200

    L14 Change repository for people

--- Homework/domain/src/main/java/repository/PersonRepository.java
+++ Homework/domain/src/main/java/repository/PersonRepository.java
@@ -1,39 +1,77 @@
 package repository;
 
 import models.Person;
+import models.Role;
 
+import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class PersonRepository {
     private static PersonRepository instance;
-    private final List<Person> people = new ArrayList<>();
+    private final Connection connection;
 
-    private PersonRepository() {
+    private PersonRepository() throws SQLException, IOException {
+        connection = Connector.getConnection();
     }
 
-    public List<Person> getPeople() {
-        return people;
-    }
-
-    public void addPerson(Person person) {
-        people.add(person);
-    }
-
-    public void deletePerson(int id) {
-        people.remove(id);
-    }
-
-    public Optional<Person> getPerson(int id) {
-        return Optional.ofNullable(people.get(id));
-    }
-
-    public static PersonRepository getInstance() {
+    public static PersonRepository getInstance() throws SQLException, IOException {
         if (instance == null) {
             instance = new PersonRepository();
         }
 
         return instance;
+    }
+
+    public List<Person> getPeople() throws SQLException {
+        List<Person> people = new ArrayList<>();
+
+        Statement statement = connection.createStatement();
+        String sql = "select * from person";
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            Person person = new Person(
+                resultSet.getInt("id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                /*resultSet.getInt("contacts"),*/
+                List.of(),
+                resultSet.getString("git_hub_nickname"),
+                Role.valueOf(resultSet.getString("role"))
+            );
+            people.add(person);
+        }
+
+        return people;
+    }
+
+    public void addPerson(Person person) throws SQLException {
+        String sql = "insert into person"
+            + "(first_name, last_name, contact_id, git_hub_nickname, role)"
+            + "values (?,?,?,?,?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, person.firstName());
+        preparedStatement.setString(2, person.lastName());
+//        preparedStatement.setInt(3, person.contacts());
+        preparedStatement.setInt(3, 1);
+        preparedStatement.setString(4, person.gitHubNickname());
+        preparedStatement.setString(5, String.valueOf(person.role()));
+        preparedStatement.execute();
+    }
+
+    public void deletePerson(int id) throws SQLException {
+        String sql = "delete from person where id=?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        preparedStatement.execute();
+    }
+
+    public Optional<Person> getPerson(int id) throws SQLException {
+        return Optional.ofNullable(getPeople().get(id));
     }
 }
