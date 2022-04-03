06524db30a549f6324aa1520e51f2c5c60acabc6
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-03 23:35:06 +0300

    L16 Add JdbcTemplate to LectionRepository

--- Homework/domain/src/main/java/repository/LectionRepository.java
+++ Homework/domain/src/main/java/repository/LectionRepository.java
@@ -1,110 +1,64 @@
 package repository;
 
-import models.HomeWork;
+import jdbc.DatabaseTemplate;
 import models.Lection;
-import models.Person;
-import models.Resource;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import javax.sql.DataSource;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class LectionRepository {
-    private final DataSource dataSource;
-    private final PersonRepository personRepository;
+    private static final NamedParameterJdbcTemplate jdbcTemplate =
+        DatabaseTemplate.getJdbcTemplate();
 
-    public LectionRepository(DataSource dataSource, PersonRepository personRepository) {
-        this.dataSource = dataSource;
-        this.personRepository = personRepository;
+    private static RowMapper<Lection> mapper() {
+        return (rs, rowNum) -> new Lection(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getString("describe"),
+            MethodsForRepository.getResourcesByLections(rs.getInt("id")),
+            MethodsForRepository.getPerson(rs.getInt("lecturer_id")).get(),
+            MethodsForRepository.getHomeWorksByLections(rs.getInt("id")),
+            rs.getTimestamp("creation_date").toLocalDateTime().toLocalDate()
+        );
     }
 
-    public List<Lection> getLections() throws SQLException {
-        List<Lection> lections = new ArrayList<>();
-
-        try (
-            Connection connection = dataSource.getConnection();
-            Statement statement = connection.createStatement()
-        ) {
-            String sql = "select * from lection";
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                int lecturerId = resultSet.getInt("lecturer_id");
-                MethodsForRepository repository = new MethodsForRepository();
-                List<Resource> resources = repository.getResourcesByLections(connection, id);
-                Optional<Person> lecturerOptional = personRepository.getPerson(lecturerId);
-                Person lecturer = null;
-                if (lecturerOptional.isPresent()) {
-                    lecturer = lecturerOptional.get();
-                }
-                List<HomeWork> homeWorks = repository.getHomeWorksByLections(connection, id);
-
-                Lection lection = new Lection(
-                    id,
-                    resultSet.getString("name"),
-                    resultSet.getString("describe"),
-                    resources,
-                    lecturer,
-                    homeWorks,
-                    resultSet.getTimestamp("creation_date").toLocalDateTime().toLocalDate()
-                );
-                lections.add(lection);
-            }
-        }
-
-        return lections;
+    public List<Lection> getLections() {
+        String sql = "SELECT * FROM lection";
+        return jdbcTemplate.query(sql, mapper());
     }
 
     public void addLection(
         Lection lection,
         int lecturerId,
         int courseId
-    ) throws SQLException {
+    ) {
         String sql = "insert into lection"
             + "(name, describe, lecturer_id, course_id)"
-            + "values (?,?,?,?)";
+            + "values (:name, :describe, :lecturer_id, :course_id)";
 
-        try (
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setString(1, lection.getName());
-            preparedStatement.setString(2, lection.getDescribe());
-            preparedStatement.setInt(3, lecturerId);
-            preparedStatement.setInt(4, courseId);
-            preparedStatement.execute();
-        }
+        Map<String, Object> param = Map.of(
+            "name", lection.getName(),
+            "describe", lection.getDescribe(),
+            "lecturer_id", lecturerId,
+            "course_id", courseId
+        );
+
+        jdbcTemplate.update(sql, param);
     }
 
-    public void deleteLection(int id) throws SQLException {
-        String sql = "delete from lection where id=?";
-
-        try (
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setInt(1, id);
-            preparedStatement.execute();
-        }
+    public void deleteLection(int id) {
+        String sql = "DELETE FROM lection WHERE id=:id";
+        Map<String, Integer> param = Map.of("id", id);
+        jdbcTemplate.update(sql, param);
     }
 
-    public Optional<Lection> getLection(int id) throws SQLException {
-        Optional<Lection> lection;
-        String sql = "select * from lection where id=?";
-
-        try (
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            MethodsForRepository repository = new MethodsForRepository();
-            lection = repository.getLection(connection, resultSet, id, personRepository);
-        }
-
-        return lection;
+    public Optional<Lection> getLection(int id) {
+        String sql = "SELECT * FROM lection WHERE id=:id";
+        Map<String, Integer> param = Map.of("id", id);
+        Lection lection = jdbcTemplate.queryForObject(sql, param, mapper());
+        return Optional.ofNullable(lection);
     }
 }
