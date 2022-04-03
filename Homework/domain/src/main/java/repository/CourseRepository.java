bbd7dfb1627cfc4eb02cdee1eeb77eef96681f8f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-03 23:34:43 +0300

    L16 Add JdbcTemplate to CourseRepository

--- Homework/domain/src/main/java/repository/CourseRepository.java
+++ Homework/domain/src/main/java/repository/CourseRepository.java
@@ -1,131 +1,48 @@
 package repository;
 
+import jdbc.DatabaseTemplate;
 import models.Course;
-import models.Lection;
-import models.Person;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import javax.sql.DataSource;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class CourseRepository {
-    private final DataSource dataSource;
-    private final PersonRepository personRepository;
+    private static final NamedParameterJdbcTemplate jdbcTemplate =
+        DatabaseTemplate.getJdbcTemplate();
 
-    public CourseRepository(DataSource dataSource, PersonRepository personRepository) {
-        this.dataSource = dataSource;
-        this.personRepository = personRepository;
+    private static RowMapper<Course> mapper() {
+        return (rs, rowNum) -> new Course(
+            rs.getInt("id"),
+            rs.getString("name"),
+            MethodsForRepository.getLectionsByCourse(rs.getInt("id")),
+            MethodsForRepository.getPeopleByCourse(rs.getInt("id"))
+        );
     }
 
-    public List<Course> getCourses() throws SQLException {
-        List<Course> courses = new ArrayList<>();
-
-        try (
-            Connection connection = dataSource.getConnection();
-            Statement statement = connection.createStatement()
-        ) {
-            String sql = "select * from course";
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                List<Lection> lections = getLections(connection, id);
-                List<Person> students = getPeople(connection, id);
-
-                Course course = new Course(id, name, lections, students);
-                courses.add(course);
-            }
-        }
-
-        return courses;
+    public List<Course> getCourses() {
+        String sql = "SELECT * FROM course";
+        return jdbcTemplate.query(sql, mapper());
     }
 
-    public void addCourse(Course course) throws SQLException {
-        String sql = "insert into course(name) values (?)";
-
-        try (
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setString(1, course.getName());
-            preparedStatement.execute();
-        }
+    public void addCourse(Course course) {
+        String sql = "INSERT INTO course(name) VALUES (:name)";
+        Map<String, Object> param = Map.of("name", course.getName());
+        jdbcTemplate.update(sql, param);
     }
 
-    public void deleteCourse(int id) throws SQLException {
-        String sql = "delete from course where id=?";
-
-        try (
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setInt(1, id);
-            preparedStatement.execute();
-        }
+    public void deleteCourse(int id) {
+        String sql = "DELETE FROM course WHERE id=:id";
+        Map<String, Integer> param = Map.of("id", id);
+        jdbcTemplate.update(sql, param);
     }
 
-    public Optional<Course> getCourse(int id) throws SQLException {
-        Course course = null;
-        String sql = "select * from course where id=?";
-
-        try (
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                String name = resultSet.getString("name");
-                List<Lection> lections = getLections(connection, id);
-                List<Person> students = getPeople(connection, id);
-                course = new Course(id, name, lections, students);
-            }
-        }
-
+    public Optional<Course> getCourse(int id) {
+        String sql = "SELECT * FROM course WHERE id=:id";
+        Map<String, Integer> param = Map.of("id", id);
+        Course course = jdbcTemplate.queryForObject(sql, param, mapper());
         return Optional.ofNullable(course);
-    }
-
-    private List<Lection> getLections(Connection connection, int id)
-        throws SQLException {
-        List<Lection> lections = new ArrayList<>();
-        String sql = "select * from lection where course_id=?";
-
-        try (
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            MethodsForRepository repository = new MethodsForRepository();
-
-            while (resultSet.next()) {
-                Optional<Lection> lection = repository
-                    .getLection(connection, resultSet, id, personRepository);
-                lection.ifPresent(lections::add);
-            }
-        }
-
-        return lections;
-    }
-
-    private List<Person> getPeople(Connection connection, int id) throws SQLException {
-        List<Person> people = new ArrayList<>();
-        String sql = "select * from person where course_id=? and role='STUDENT'";
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                MethodsForRepository repository = new MethodsForRepository();
-                Optional<Person> person = repository.getPerson(connection, resultSet, id);
-                person.ifPresent(people::add);
-            }
-        }
-
-        return people;
     }
 }
