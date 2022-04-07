aba2f92ee19270c132bc674487ecffa4d2733959
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-07 18:05:12 +0300

    L17 Include Spring Boot

---
+++ Homework/web-app/src/main/java/org/geekhub/web/servlets/ApplicationStarter.java
@@ -0,0 +1,12 @@
+package org.geekhub.web.servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
