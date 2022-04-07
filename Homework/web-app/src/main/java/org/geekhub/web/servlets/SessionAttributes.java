2af0196298f0d8c895dfb5ab8b13d6a487730780
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-08 00:47:16 +0300

    L17 Refactor web-app

--- Homework/web-app/src/main/java/org/geekhub/web/servlets/SessionAttributes.java
+++ Homework/web-app/src/main/java/org/geekhub/web/servlets/SessionAttributes.java
@@ -2,20 +2,4 @@ package org.geekhub.web.servlets;
 
 public class SessionAttributes {
     public static final String USER_NAME_SESSION_PARAMETER = "userName";
-    public static final String LOGGER_STORAGE_SESSION_PARAMETER = "loggerStorage";
-    public static final String COMMAND_SESSION_PARAMETER = "command";
-    public static final String ID_SESSION_PARAMETER = "id";
-    public static final String NAME_SESSION_PARAMETER = "name";
-    public static final String TASK_SESSION_PARAMETER = "task";
-    public static final String DATE_SESSION_PARAMETER = "date";
-    public static final String TIME_SESSION_PARAMETER = "time";
-    public static final String DESCRIBE_SESSION_PARAMETER = "describe";
-    public static final String LECTURER_ID_SESSION_PARAMETER = "lecturerName";
-    public static final String TYPE_SESSION_PARAMETER = "type";
-    public static final String DATA_SESSION_PARAMETER = "data";
-    public static final String SURNAME_SESSION_PARAMETER = "surname";
-    public static final String NICKNAME_SESSION_PARAMETER = "nickname";
-    public static final String ROLE_SESSION_PARAMETER = "role";
-    public static final String EMAIL_SESSION_PARAMETER = "email";
-    public static final String PHONE_SESSION_PARAMETER = "phone";
 }
