517c383fad1afd52263ad7b6b55a4d4537b3a8e5
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-08 21:09:25 +0300

    L17 Refactor AuthorisationController

--- Homework/web-app/src/main/java/org/geekhub/web/controllers/AuthorisationController.java
+++ Homework/web-app/src/main/java/org/geekhub/web/controllers/AuthorisationController.java
@@ -1,17 +1,14 @@
 package org.geekhub.web.controllers;
 
-import config.DatabaseConfig;
-import org.flywaydb.core.Flyway;
-import org.geekhub.web.SessionAttributes;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+
+import static org.geekhub.web.SessionAttributes.USER_NAME_SESSION_PARAMETER;
 
 @Controller
 public class AuthorisationController {
@@ -27,16 +24,10 @@ public class AuthorisationController {
     ) {
         List<String> users = List.of("admin", "user");
         if (users.contains(userName)) {
-            session.setAttribute(SessionAttributes.USER_NAME_SESSION_PARAMETER, userName);
+            session.setAttribute(USER_NAME_SESSION_PARAMETER, userName);
             return "redirect:/";
         } else {
             return "redirect:/authorisation";
         }
-    }
-
-    @DeleteMapping("/authorisation")
-    public String logOut(HttpSession session) {
-        session.setAttribute(SessionAttributes.USER_NAME_SESSION_PARAMETER, null);
-        return "redirect:/authorisation";
     }
 }
